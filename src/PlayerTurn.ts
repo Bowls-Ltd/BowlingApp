c7c5f1ee4190fb17bc582fe11db8fa14f6e983b0
Damien <damiensporting@gmail.com>
2022-01-05 22:48:49 +0100

    reformating PlayerTurn

--- src/PlayerTurn.ts
+++ src/PlayerTurn.ts
@@ -13,18 +13,10 @@ class PlayerTurn {
     addPins(pinsNb: number): void {
         if (pinsNb < 0 || pinsNb > 10)
             throw new Error("invalid input")
-        if ( ! Number.isInteger(pinsNb))
+        if (!Number.isInteger(pinsNb))
             throw new Error("invalid input")
         if (pinsNb === null || pinsNb === undefined)
             throw new Error("invalid input")
-        if ( ! this.isLastTurn && this.shots.length === 2)
-            throw new Error("cannot play more than twice")
-        if ( ! this.isLastTurn && this.isStrike())
-            throw new Error("cannot play more than twice")
-        if (this.isLastTurn && this.shots.length === 2 && !this.isStrike() && !this.isSpare())
-            throw new Error("cannot play more than twice")
-        if (this.isLastTurn && this.shots.length === 3)
-            throw new Error("cannot play more than thrice")
         if (pinsNb > this.remainingPins)
             throw new Error("total pins down cannot exceed 10")
 
@@ -33,18 +25,15 @@ class PlayerTurn {
         if(this.isLastTurn) {
             if(this.isStrike() && this.shots.length === 1)
                 this.remainingPins = 10
-            else if(this.shots.length === 2) {
+            if(this.shots.length === 2) {
                 if(this.isStrike() && this.shots[1] === 10)
                     this.remainingPins = 10
-                else if(this.isSpare())
+                if(this.isSpare())
                     this.remainingPins = 10
-                else
-                    this.remainingPins -= pinsNb 
-            } else
-                this.remainingPins -= pinsNb   
-        } else {
-            this.remainingPins -= pinsNb       
+            }
         }
+                
+        this.remainingPins -= pinsNb       
     }
 
     getShots() {
@@ -60,43 +49,19 @@ class PlayerTurn {
     }
 
     isOver(): boolean {
-        if (this.shots.length == 0) {
-            return false
-        } else if (this.shots.length == 1) {
-            if (this.isLastTurn) {
-                return false
-            } else {
-                if(this.isStrike()) {
-                    return true
-                } else {
-                    return false
-                }
-            }
-        } else if (this.shots.length == 2) {
-            if (this.isLastTurn) {
-                if (this.isStrike()) {
-                    return false
-                } else if(this.isSpare()) {
-                    return false
-                } else {
-                    return true
-                }
-            } else {
-                return true
-            }
-        } else {
-            return true
-        }
+        if (this.isLastTurn)
+            return this.shots.length === 3
+        else
+            return this.shots.length === 2
     }
 
     isStrike(): boolean {
         if (this.shots.length >= 1)
             return this.shots[0] === 10
-        else return false
     }
 
     isSpare(): boolean {
-        if (this.shots.length >= 2 && this.shots[0] + this.shots[1] === 10)
+        if (this.shots.length === 2 && this.pinsSum() === 10)
             return true
         else
             return false
