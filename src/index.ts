93a4eb0862ae7d12810680a2ff7f181cb26f2240
Damien <damiensporting@gmail.com>
2022-01-08 00:39:26 +0100

    index main loop

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
+import {PlayerTurn} from "./PlayerTurn"
+import {Player} from "./Player"
+import {PlayerView} from "./PlayerView"
 
+// we have to wait for the html to be fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  let size : number = 2;
+  let Players : Array<Player> = new Array(size);
+  let PlayersView : Array<PlayerView> = new Array(size);
+
+  for (let i = 0; i < Players.length; i++) {
+
+    Players[i] = new Player("Player "+ (i+1));
+
+    let UITurn : HTMLElement = document.getElementById("player_" + (i+1) + "_turn");
+    let UIScore : HTMLElement = document.getElementById("player_" + (i+1) + "_score");
+    PlayersView[i] = new PlayerView(UITurn, UIScore);
+  }
+
+  for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < Players.length; j++) {
+        Players[j].playTurn();
+        PlayersView[j].update(Players[j].getTurns(), Players[j].computeAccumulatedScores());
+    }
+  }
+})
