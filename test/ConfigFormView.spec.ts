d7f909e25be06bd6d953178a02d051db71278187
Atlas974 <ryan.persee@gmail.com>
2022-02-06 15:54:05 +0100

    Updated ConfigFormView.spec.ts

--- test/ConfigFormView.spec.ts
+++ test/ConfigFormView.spec.ts
@@ -2,47 +2,91 @@
  * @jest-environment jsdom
  */
 
-import fs from 'fs'
-import path from 'path'
+import * as fs from 'fs'
+import * as path from 'path'
 
 import {
     fireEvent,
-    getByLabelText,
-    getByText,
-    getByTestId,
-    queryByTestId,
+    // getByLabelText,
+    // getByText,
+    // getByTestId,
+    // queryByTestId,
     // Tip: all queries are also exposed on an object
     // called "queries" which you could import here as well
-    waitFor,
-  } from '@testing-library/dom'
-  // adds special assertions like toHaveTextContent
-  import '@testing-library/jest-dom'
+    // waitFor,
+} from '@testing-library/dom'
+// adds special assertions like toHaveTextContent
+import '@testing-library/jest-dom'
 
 import {ConfigFormView} from "../src/ConfigFormView"
-import {Game} from "../src/Game"
+// import {Game} from "../src/Game"
 import {initGame} from "../src/Init"
 
 const html = fs.readFileSync(path.resolve(__dirname, "../src/index.html"))
 
-describe.skip('ConfigFormView', () => {
-    let view, divForm, nplayersTitle, nplayersInput, npinsTitle, npinsInput, button, error
-    beforeEach(() => {
-        document.documentElement.innerHTML = html.toString()
-        const mainContainer = document.getElementById('main-container')
-        view = new ConfigFormView(mainContainer)
-        view.attachGameCreationCallback(initGame)
+describe('ConfigFormView', () => {
+    let view, configForm, button, error,
+        nplayersTitle, nplayersInput, decrPlayersButton, incrPlayersButton,
+        npinsTitle, npinsInput, decrPinsButton, incrPinsButton;
 
-        divForm = (document.querySelector('#config-form'))
-        nplayersTitle = divForm.querySelector('#nb-players-title')
-        nplayersInput = divForm.querySelector('#nb-players-input')
-        npinsTitle = divForm.querySelector('#nb-pins-title')
-        npinsInput = divForm.querySelector('#nb-pins-input')
-        button = divForm.querySelector('#validate-div')
-        error = divForm.querySelector('#config-error-box')
+    function setPinsInput(target: number) {
+        let current: number = parseInt(npinsInput.innerHTML);
+        let clicks: number = Math.abs(target - current);
+        let button = (target - current > 0) ? incrPinsButton : decrPinsButton;
+        for (let i = 0; i < clicks; i++) {
+            fireEvent(button, new MouseEvent('click'));
+        }
+        expect(parseInt(npinsInput.innerHTML)).toBe(target);
+    }
+
+    function setPlayerInput(target: number) {
+        let current: number = parseInt(nplayersInput.innerHTML);
+        let clicks: number = Math.abs(target - current);
+        let button = (target - current > 0) ? incrPlayersButton : decrPlayersButton;
+        for (let i = 0; i < clicks; i++) {
+            fireEvent(button, new MouseEvent('click'));
+        }
+        expect(parseInt(nplayersInput.innerHTML)).toBe(target);
+    }
+
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        const mainContainer = document.getElementById('main-container');
+        view = new ConfigFormView(mainContainer);
+        view.attachGameCreationCallback(initGame);
+
+        configForm = document.querySelector('#config-form');
+        expect(configForm).not.toBeNull();
+
+        nplayersTitle = document.querySelector('#config-form > div:nth-child(2) > div:nth-child(1)');
+        expect(nplayersTitle).not.toBeNull();
+
+        decrPlayersButton = document.querySelector('#config-form > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)');
+        expect(decrPlayersButton).not.toBeNull();
+        nplayersInput = document.querySelector('#config-form > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)');
+        expect(parseInt(nplayersInput.innerHTML)).toBe(2);
+        incrPlayersButton = document.querySelector('#config-form > div:nth-child(2) > div:nth-child(2) > div:nth-child(3)');
+        expect(incrPlayersButton).not.toBeNull();
+
+        npinsTitle = document.querySelector('#config-form > div:nth-child(3) > div:nth-child(1)');
+        expect(npinsTitle).not.toBeNull();
+
+        decrPinsButton = document.querySelector('#config-form > div:nth-child(3) > div:nth-child(2) > div:nth-child(1)');
+        expect(decrPinsButton).not.toBeNull();
+        npinsInput = document.querySelector('#config-form > div:nth-child(3) > div:nth-child(2) > div:nth-child(2)');
+        expect(parseInt(npinsInput.innerHTML)).toBe(10);
+        incrPinsButton = document.querySelector('#config-form > div:nth-child(3) > div:nth-child(2) > div:nth-child(3)');
+        expect(incrPinsButton).not.toBeNull();
+
+        button = document.querySelector('#validate-div');
+        expect(button).not.toBeNull();
+
+        error = document.querySelector('#config-error-box');
+        expect(error).not.toBeNull();
     });
 
     test('Constructor', () => {
-        expect(divForm).toBeDefined()
+        expect(configForm).toBeDefined()
 
         expect(nplayersTitle).toBeDefined()
         expect(nplayersTitle.innerHTML).toBe('Nombre de joueurs')
@@ -63,56 +107,69 @@ describe.skip('ConfigFormView', () => {
     })
 
     test('Valid form', () => {
-        nplayersInput.innerHTML = "5"
-        npinsInput.innerHTML = "5"
-        fireEvent(button, new MouseEvent('click'))
-        expect(document.querySelector('#config-form')).toBe(null)
+        setPlayerInput(5);
+        setPinsInput(5);
+        fireEvent(button, new MouseEvent('click'));
+        expect(document.querySelector('#config-form')).toBeNull();
     })
 
     test('One player', () => {
-        nplayersInput.innerHTML = "1"
-        npinsInput.innerHTML = "5"
+        setPlayerInput(1);
+        setPinsInput(5);
         fireEvent(button, new MouseEvent('click'))
-        expect(document.querySelector('#config-form')).toBe(null)
+        expect(document.querySelector('#config-form')).toBeNull();
     })
 
-    test('Negative player number', () => {
-        nplayersInput.innerHTML = "-5"
-        npinsInput.innerHTML = "5"
+    test.skip('Negative player number', () => {
+        setPlayerInput(-5);
+        setPinsInput(5);
         fireEvent(button, new MouseEvent('click'))
         expect(error.style.visibility).toBe('visible')
         expect(error.innerHTML).toBe('Error: Invalid parameter : nbPlayers must be positive')
     })
 
-    test('Negative pin number', () => {
-        nplayersInput.innerHTML = "5"
-        npinsInput.innerHTML = "-5"
+    test.skip('Negative pin number', () => {
+        setPlayerInput(5);
+        setPinsInput(-5);
         fireEvent(button, new MouseEvent('click'))
         expect(error.style.visibility).toBe('visible')
         expect(error.innerHTML).toBe('Error: Invalid parameter : nbPins must be positive')
     })
 
-    test('Negative player and pin number', () => {
-        nplayersInput.innerHTML = "-5"
-        npinsInput.innerHTML = "-5"
+    test.skip('Negative player and pin number', () => {
+        setPlayerInput(-5);
+        setPinsInput(-5);
         fireEvent(button, new MouseEvent('click'))
         expect(error.style.visibility).toBe('visible')
         expect(error.innerHTML).toBe('Error: Invalid parameter : nbPlayers must be positive')
     })
 
-    test('Zero player', () => {
-        nplayersInput.innerHTML = "0"
-        npinsInput.innerHTML = "5"
+    test.skip('Zero player', () => {
+        setPlayerInput(0);
+        setPinsInput(5);
         fireEvent(button, new MouseEvent('click'))
         expect(error.style.visibility).toBe('visible')
         expect(error.innerHTML).toBe('Error: Invalid parameter : nbPlayers must be positive')
     })
 
-    test('Zero pins', () => {
-        nplayersInput.innerHTML = "5"
-        npinsInput.innerHTML = "0"
+    test.skip('Zero pins', () => {
+        setPlayerInput(5);
+        setPinsInput(0);
         fireEvent(button, new MouseEvent('click'))
         expect(error.style.visibility).toBe('visible')
         expect(error.innerHTML).toBe('Error: Invalid parameter : nbPins must be positive')
+    })
+
+    test('GameCreationCallback', () => {
+        let actualPinCount: number;
+        view.attachGameCreationCallback((view_, nbPlayers_, nbPins_) => {
+            actualPinCount = nbPins_;
+        });
+
+        for (let i = 10; i > 0; i--) {
+            setPinsInput(i);
+            fireEvent(button, new MouseEvent('click'));
+            expect(actualPinCount).toBe(i);
+        }
     })
 })
