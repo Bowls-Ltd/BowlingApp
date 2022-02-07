b830579e26d34d5a7f09638a960f0e5265d8d103
G7r7 <72744587+G7r7@users.noreply.github.com>
2022-02-07 11:06:36 +0100

    css minifier and production mode (#70)
    * css minifier and production mode
    
    * v2.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.ts',
   module: {
     rules: [
       {
         use: [{loader: 'ts-loader', options: {onlyCompileBundledFiles: true}}],
       },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
     extensions: ['.ts'],
   },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
         {
